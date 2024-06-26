8bab6fdb6ebc188df8ad668aadb5ad7c41c34d05
Daniel Castillo <dev@dcastillogi.com>
2024-06-26 17:59:53 -0500

    padding

--- src/app/(public)/subscription/[number]/page.tsx
+++ src/app/(public)/subscription/[number]/page.tsx
@@ -8,7 +8,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 export default function SubscriptionPage() {
     return (
         <div className="lg:flex lg:justify-end">
-            <div className="lg:w-1/2 px-4 pb-20 lg:pb-10 py-10 bg-muted/30 lg:h-screen top-0 left-0 lg:fixed">
+            <div className="lg:w-1/2 px-6 pb-20 lg:pb-10 py-10 bg-muted/30 lg:h-screen top-0 left-0 lg:fixed">
                 <div className="max-w-lg mx-auto h-full">
                     <div className="h-16">
                         <h1 className="text-3xl font-semibold tracking-tight">
@@ -18,7 +18,7 @@ export default function SubscriptionPage() {
                     <SubscriptionDetails />
                 </div>
             </div>
-            <div className="lg:w-1/2 px-4 py-10 h-screen">
+            <div className="lg:w-1/2 px-6 py-10 h-screen">
                 <div className="max-w-lg mx-auto h-full">
                     <div className="h-16">
                         <h2 className="text-2xl font-semibold tracking-tight">
